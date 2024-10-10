d3eebb0c1077f7a66b00c98c2171bc35a0b50953
JacoboPBV <jpitabaamondev23@fpcoruna.afundacion.org>
2024-10-10 13:39:06 +0200

    Comentarios añadidos

--- Tesoro.py
+++ Tesoro.py
@@ -2,16 +2,18 @@ from random import randint
 
 
 class Tesoro:
-    beneficios:list[str] = list()
+    beneficios:list[str] = list() #Lista de beneficios
 
     def __init__(self):
+        #Inicializamos la lista con los 3 valores predeterminados
         self.beneficios.append("Ataque")
         self.beneficios.append("Defensa")
         self.beneficios.append("Curación")
 
     def encontrar_tesoro(self, heroe):
-        beneficio = self.beneficios.__getitem__(randint(0,2))
+        beneficio = self.beneficios.__getitem__(randint(0,2)) #Obtenemos uno de los beneficios de manera aleatoria con randint
         print("El héroe ha encontrado un tesoro: " + beneficio)
+        #Le asignamos el beneficio al héroe dependiendo del valor obtenido
         if beneficio == "Ataque":
             heroe.ataque += 3
             print("El ataque de " + heroe.nombre + " aumenta a " + str(heroe.ataque))
