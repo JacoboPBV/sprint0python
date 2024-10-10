2962a67cb05ab575edf99a389b882223c4668ff8
JacoboPBV <jpitabaamondev23@fpcoruna.afundacion.org>
2024-10-10 12:01:55 +0000

    Mejorado 'switch' con un while y un try-except

--- calculadora.py
+++ calculadora.py
@@ -2,18 +2,24 @@ from operaciones import suma, resta, multiplicacion, division
 
 def main():
     print("Qué operación deseas realizar? ")
-    res = input("Suma (S) / Resta (R) / Multiplicación (M) / División (D)")
+    res = input("Suma (S) / Resta (R) / Multiplicación (M) / División (D): ")
     
-    #"Switch" de las posibles elecciones
-    if res in ["S", "s"]:
-        suma(int(input("Primer número: ")), int(input("Segundo número: ")))
-    elif res in ["R", "r"]:
-        resta(int(input("Primer número: ")), int(input("Segundo número: ")))
-    elif res in ["M", "m"]:
-        multiplicacion(int(input("Primer número: ")), int(input("Segundo número: ")))
-    elif res in ["D", "d"]:
-        division(int(input("Primer número: ")), int(input("Segundo número: ")))
-    else: print("Opción no válida")
+    error = True
+    while error: #Mientras no ponga números
+        #"Switch" de las posibles elecciones
+        try: #Para que solo ponga números
+            if res in ["S", "s"]:
+                suma(int(input("Primer número: ")), int(input("Segundo número: ")))
+            elif res in ["R", "r"]:
+                resta(int(input("Primer número: ")), int(input("Segundo número: ")))
+            elif res in ["M", "m"]:
+                multiplicacion(int(input("Primer número: ")), int(input("Segundo número: ")))
+            elif res in ["D", "d"]:
+                division(int(input("Primer número: ")), int(input("Segundo número: ")))
+            else: print("Opción no válida")
+
+            error = False
+        except ValueError: error = True
 
 main() #Ejecutamos el main
 res = input("Desea hacer más operaciones? (S/N)")
